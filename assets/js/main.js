4e65996fffe580b34462522ab48d3e6a98ee2cac
Antonio <antonio.lovotrico@icloud.com>
2020-11-13 17:32:47 +0100

    exsercise completed

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+//richiesta al cliente dei km che intende percorrere
+var km = Number(prompt("quanti Kilometri vuoi percorrere?"));
+
+//richiesta al cliente della età anagrafica
+var  yearsOld = Number(prompt("quanti anni hai?"));
+
+// definizione del costo kilometrico
+var costoKm = (km * 21) / 100;
+//  console.log(costoKm);
+
+ //istruzioni condizionali
+var priceOver = (costoKm / 100) * 40 ;
+var priceUnder = (costoKm / 100) * 20;
+// console.log(priceUnder);
+ // scontistica 
+ if (yearsOld >= 65) {
+     document.getElementById("costo").innerHTML = costoKm - priceOver + "€";
+ } else if (yearsOld < 18) {
+     document.getElementById("costo").innerHTML = costoKm - priceUnder + "€";
+ } else {
+     document.getElementById("costo").innerHTML = costoKm + "€";
+ }
